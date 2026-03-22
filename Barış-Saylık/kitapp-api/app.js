58f69ab52955d395fe8b8b4ed1d8fbf3c91c2c67
gfbaris <barissaylik247@gmail.com>
2026-03-22 13:32:19 +0300

    fix: CORS sorunu çözüldü

--- Barış-Saylık/kitapp-api/app.js
+++ Barış-Saylık/kitapp-api/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const cors = require('cors');
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
